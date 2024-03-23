af8303af68d45e614f9a209b3b87a0315e936fa7
khalidnbg <khalidnabgaoui4@gmail.com>
2024-03-23 13:15:58 +0000

    create the abot page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-4xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Here are three paragraphs you can use for the about page of your movie
+        app: 1. Our movie app is designed to bring the magic of cinema right to
+        your fingertips. Whether you're a film enthusiast or just looking for
+        your next movie night pick, our app has you covered. With a vast
+        collection of movies from all genres and eras, you can explore and
+        discover new favorites with ease.
+      </p>
+      <p>
+        We believe that watching movies should be a seamless and enjoyable
+        experience. That`s why our app is designed to be user-friendly and
+        intuitive, allowing you to browse, search, and watch movies
+        effortlessly. Whether you`re on your couch at home or on the go, our app
+        ensures that you have access to quality entertainment whenever you want
+        it.
+      </p>
+      <p>
+        Our team is passionate about movies and is dedicated to providing you
+        with the best possible movie-watching experience. We are constantly
+        updating our collection to include the latest releases and timeless
+        classics, ensuring that there is always something new and exciting to
+        watch. Join us on this cinematic journey and let our app be your guide
+        to the world of movies.
+      </p>
+    </div>
+  );
+}
--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,17 +1,16 @@
 "use client";
 
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { MdLightMode, MdDarkMode } from "react-icons/md";
 
 export default function DarkModeSwitch() {
   const { theme, setTheme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
   const currentTheme = theme === "system" ? systemTheme : theme;
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  useEffect(() => setMounted(true), []);
 
   return (
     <div>
