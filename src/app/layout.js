53fa43bc5095809fbb280ae203797e1b897161ab
khalidnbg <khalidnabgaoui4@gmail.com>
2024-03-23 12:47:53 +0000

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Providers from "@/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,12 +10,15 @@ export const metadata = {
   description: "This is a movie db clone",
 };
 
+// RootLayout component
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
